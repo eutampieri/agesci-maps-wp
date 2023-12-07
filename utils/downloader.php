0201a2dcb3b791fc848f7a7d6cae5f0a8c2a7ea7
Eugenio Tampieri <webmaster@emiro.agesci.it>
2023-12-07 16:07:15 +0100

    WIP pmtiles auto updater

--- utils/downloader.php
+++ utils/downloader.php
@@ -13,17 +13,28 @@ function detect_os_arch() {
 	}
 	return [$os, $arch];
 }
+function download_pmtiles($path) {
+	$os_arch = detect_os_arch();
+	$os = $os_arch[0];
+	$arch = $os_arch[1];
+	$url = "https://github.com/protomaps/go-pmtiles/releases/latest/download/go-pmtiles_1.11.1_".$os."_".$arch.".tar.gz";
+	try {
+		$zip = file_get_contents($url);
+
+	} catch (\Throwable $th) {
+		//throw $th;
+	}
+}
+
 function ensure_pmtiles($path) {
-    if(!is_file($path)) {
-        $os_arch = detect_os_arch();
-        $os = $os_arch[0];
-        $arch = $os_arch[1];
-        $url = "";
-        try {
-            $zip = file_get_contents($url);
-            
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
-    }
+	if(!is_file($path)) {
+		download_pmtiles($path);
+	} else {
+		$version = explode(" ", exec($path." version"))[1];
+		$new_version = json_decode(file_get_contents("https://api.github.com/repos/protomaps/go-pmtiles/releases/latest"), true)["name"];
+		$new_version = str_replace("v", "", $new_version);
+		if(version_compare($new_version, $version) > 0) {
+			download_pmtiles($path);
+		}
+	}
 }
