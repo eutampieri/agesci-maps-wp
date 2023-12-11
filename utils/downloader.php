61b2e1776a21336ce40e42a95327bedf99a9459a
Eugenio Tampieri <webmaster@emiro.agesci.it>
2023-12-11 10:26:02 +0100

    Fixed downloader

--- utils/downloader.php
+++ utils/downloader.php
@@ -31,22 +31,31 @@ function download_pmtiles($path, $version) {
 }
 
 function ensure_pmtiles($path) {
-	if(!is_file($path)) {
-		download_pmtiles($path);
-	} else {
-		$version = explode(" ", exec($path." version"))[1];
-		$opts = [
-		        'http' => [
+	$opts = [
+	        'http' => [
 	                'method' => 'GET',
 	                'header' => ['User-Agent: pmtiles_downloader github.com/eutampieri/agesci-maps-wp']
-	        	]
-		];
+        	]
+	];
 
-		$context = stream_context_create($opts);
-		$new_version = json_decode(file_get_contents("https://api.github.com/repos/protomaps/go-pmtiles/releases/latest", false, $context), true)["name"];
-		$new_version = str_replace("v", "", $new_version);
+	$context = stream_context_create($opts);
+	$new_version = json_decode(file_get_contents("https://api.github.com/repos/protomaps/go-pmtiles/releases/latest", false, $context), true)["name"];
+	$new_version = str_replace("v", "", $new_version);
+	if(!is_file($path)) {
+		download_pmtiles($path, $new_version);
+	} else {
+		$version = explode(" ", exec($path." version"))[1];
+
 		if(version_compare($new_version, $version) > 0) {
 			download_pmtiles($path, $new_version);
 		}
 	}
+}
+
+function download_map($region) {
+	$builds = json_decode(file_get_contents("https://build-metadata.protomaps.dev/builds.json"), true);
+	$build = $builds[count($builds) - 1]["key"];
+	$pmtiles = dirname(dirname(__FILE__))."/pmtiles";
+	ensure_pmtiles($pmtiles);
+	exec($pmtiles." extract https://build.protomaps.com/".$build." \"".ABSPATH."/wp-content/uploads/map.pmtiles\" --region=\"".dirname(dirname(__FILE__))."/regions/".$region.".json\"");
 }
