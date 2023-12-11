7146a6d53361774ebcad78ff5c7a6ddc069ef9a5
Eugenio Tampieri <webmaster@emiro.agesci.it>
2023-12-11 08:49:41 +0100

    Fix get_regions()

--- utils/regions.php
+++ utils/regions.php
@@ -8,8 +8,8 @@ function get_regions() {
 	$result = [];
 	foreach(glob($folder."/*.json") as $file) {
 		$name = pathinfo($file, PATHINFO_FILENAME);
-		$geojson = file_get_contents($file);
-		$value = $geogjson["features"][0]["properties"]["reg_name"];
+		$geojson = json_decode(file_get_contents($file), true);
+		$value = $geojson["features"][0]["properties"]["reg_name"];
 		$result[$name] = $value;
 	}
 	return $result;
